5333ccd78ab811e4f14a91c24287140e65415005
anton <anton@Lenovo-B570>
2017-03-19 23:17:00 +0500

    Git init

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=crm',
+    'username' => 'crm',
+    'password' => 'eGvDmQsEk5vgRY7p',
+    'charset' => 'utf8',
+];
