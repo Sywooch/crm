2cbdcc193f51eef63d55589d649b4a5dd00a2c98
Anton Kazarinov <askazarinov@gmail.com>
2017-06-05 13:02:08 +0500

    Изменение конфигураций
    1. Изменен пароль в БД
    2. Изменена конфигурация отправки почты

--- config/db.php
+++ config/db.php
@@ -4,6 +4,6 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=crm',
     'username' => 'crm',
-    'password' => 'eGvDmQsEk5vgRY7p',
+    'password' => 'aa53847f4d1e985dbe23f332470e3a61',
     'charset' => 'utf8',
 ];
