1a2f8346b9fc444c684d923f631eb08b95e71d32
Anton Kazarinov <kazarinov@itisinfo.ru>
2017-06-05 14:23:04 +0500

    Исправление ошибок
    Исправлены ссылки для перехода к форме восстановления пароля на абсолютные

--- mail/new_user.php
+++ mail/new_user.php
@@ -4,4 +4,4 @@ use yii\helpers\Html;
 
 ?>
 <p>Здравствуйте, <?= $firstname ?> <?= $patronimyc ?>!</p>
-<p>Для Вас в системе CRM «ИТИС» была создана новая учетная запись. Для входа перейдите по этой <?= Html::a('ссылке', Url::to(['site/reset-password', 'token' => $token]))?></p>
+<p>Для Вас в системе CRM «ИТИС» была создана новая учетная запись. Для входа перейдите по этой <?= Html::a('ссылке', Url::to(['site/reset-password', 'token' => $token], true))?></p>
--- mail/request_token.php
+++ mail/request_token.php
@@ -4,4 +4,4 @@ use yii\helpers\Html;
 
 ?>
 <p>Здравствуйте, <?= $firstname ?> <?= $patronimyc ?>!</p>
-<p>Для восстановления пароля перейдите по этой <?= Html::a('ссылке', Url::to(['site/reset-password', 'token' => $token]))?></p>
+<p>Для восстановления пароля перейдите по этой <?= Html::a('ссылке', Url::to(['site/reset-password', 'token' => $token], true))?></p>
