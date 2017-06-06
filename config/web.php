20d9c6dbe3d351e77a730b784a51876347fd970a
Anton Kazarinov <kazarinov@itisinfo.ru>
2017-06-06 14:05:37 +0500

    Добавлен IP компании в модуль Debug и Gii

--- config/web.php
+++ config/web.php
@@ -112,14 +112,14 @@ if (YII_ENV_DEV) {
     $config['modules']['debug'] = [
         'class' => 'yii\debug\Module',
             // uncomment the following to add your IP if you are not connecting from localhost.
-            //'allowedIPs' => ['127.0.0.1', '::1'],
+            'allowedIPs' => ['127.0.0.1', '194.67.32.214'],
     ];
 
     $config['bootstrap'][] = 'gii';
     $config['modules']['gii'] = [
         'class' => 'yii\gii\Module',
             // uncomment the following to add your IP if you are not connecting from localhost.
-            //'allowedIPs' => ['127.0.0.1', '::1'],
+            'allowedIPs' => ['127.0.0.1', '194.67.32.214'],
     ];
 }
 
